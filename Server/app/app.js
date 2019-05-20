e105cf0110155d90d6f5f0e060656f64adab3fc5
SimoneTufariello <tufariello.simone@iisgalvanimi.edu.it>
2019-05-20 11:58:02 +0000

    correzione variabili e connesione

--- Server/app/app.js
+++ Server/app/app.js
@@ -16,9 +16,6 @@ var conn = mysql.createConnection({
     user: "UPHEOeV4ml"
     });
 
-app.set('views', './views');
-app.set('view engine', 'pug');
-
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -26,7 +23,33 @@ app.get('/', function (req, res) {
 
 app.post('/register', function(req, res) {
 
-    var nome = req.body.nome;
+    var nome = req.query.nome;
+    var cognome = req.query.cognome;
+    var username = req.query.username;
+    var email = req.query.email;
+    var data = req.query.data;
+    var pass = req.query.password;
+
+    var stat; //no
+
+
+
+    conn.connect(function(err) {
+        if (err) throw err;
+        conn.query('INSERT INTO User(Nome, Cognome, Username, Password, Email, Data) VALUES("' + nome + '","' + cognome + '","' + username + '","' + pass + '","' + email + '","' + data + '")', function(err, result, fields) {
+            if (err) throw err;
+            console.log("OK, registrazione effettuata correttamente: " + nome +" "+ pass);
+
+            res.send(true);
+        });
+    });
+})
+
+app.get('/login', function(req, res) {
+
+
+    res.send('Ciao');
+   /* var nome = req.body.nome;
     var cognome = req.body.cognome;
     var username = req.body.username;
     var email = req.body.email;
@@ -45,8 +68,9 @@ app.post('/register', function(req, res) {
 
             res.send(true);
         });
-    });
+    }); */
 })
+
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
